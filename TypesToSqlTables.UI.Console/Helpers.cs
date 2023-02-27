4fd37a0a07bab6c6f0cbb409329a9e6be96dd9e6
caterryan <caterryan1@gmail.com>
2023-02-27 13:07:19 -0600

    Add error handling for parameter inputs.
    Add Helper class with GetInputParameters to request input if received incorrect number of parameters.

---
+++ TypesToSqlTables.UI.Console/Helpers.cs
@@ -0,0 +1,43 @@
+﻿namespace TypesToSqlTables.UI.ConsoleCli;
+
+internal static class Helpers
+{
+
+    internal static (string assemblyPath, string schemaName) GetInputParameters(string[] args)
+    {
+        string assemblyPath;
+        string schemaName;
+
+        if (args.Length == 2)
+        {
+            assemblyPath = args.FirstOrDefault();
+            schemaName = args[1];
+        }
+        else
+        {
+            Console.WriteLine(
+                $"""
+                Invalid number of input parameters.
+                Number of parameters provided: {args.Length}
+                
+                Required Parameters: 
+                  - Path to .dll Assembly
+                  - Schema Name
+
+                """);
+
+            Console.Write("Assembly Path: ");
+            assemblyPath = Console.ReadLine();
+
+            Console.Write("Schema Name: ");
+            schemaName = Console.ReadLine();
+        }
+
+        if (schemaName == "" || schemaName == null)
+        {
+            throw new ArgumentException("Invalid input: Schema Name cannot be empty or null");
+        }
+
+        return (assemblyPath, schemaName);
+    }
+}
